6ebac94b22d6405dce1754bdbc04a7eae713f16d
milen-vm <milen.milev@protonmail.com>
2025-09-12 23:23:16 +0300

    Added migratins and models.

---
+++ database/migrations/2025_09_12_194701_create_experiences_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('experiences', function (Blueprint $table) {
+            $table->id();
+            $table->string('position');
+            $table->longText('short_description');
+            $table->date('date_from');
+            $table->date('date_to');
+            $table->string('company_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('experiences');
+    }
+};
---
+++ database/migrations/2025_09_12_200902_create_experience_technology_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('experience_technology', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('experience_id')->nullable(false);
+            $table->unsignedBigInteger('technology_id')->nullable(false);
+            
+            $table->foreign('experience_id')->references('id')->on('experiences')->onDelete('cascade');
+            $table->foreign('technology_id')->references('id')->on('experiences')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('experience_technology');
+    }
+};
