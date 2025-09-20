e37c3e27cb864ae9b52234223fcd8cac6a67a578
milen-vm <milen.milev@protonmail.com>
2025-09-20 21:01:26 +0300

    Create Prijects api.

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ProjectRequest;
+use App\Http\Resources\ProjectResource;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return ProjectResource::collection(Project::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ProjectRequest $request)
+    {
+        $data = $request->only(['title', 'description', 'link']);
+        $data['image'] = $request->file('image')->store('images', 'public');
+
+        $project = Project::create($data);
+
+        return new ProjectResource($project);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Project $project)
+    {
+        return new ProjectResource($project);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
