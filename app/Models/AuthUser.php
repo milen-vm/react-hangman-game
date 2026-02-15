a10d0ba84d186f46aabf5dc266582df2c1b745f1
milen-vm <milen.milev@protonmail.com>
2026-02-15 17:36:01 +0200

    Added auth user model.

---
+++ app/Models/AuthUser.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AuthUser extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'email', 'password', 'name',
+    ];
+
+    protected $table = 'auth_users';
+}
